d04a698c5aee23b5d7a3d90581a2deb8dae5c1c3
leejuhyeon <esloven3@gmail.com>
2021-06-20 23:20:26 +0900

    Update

--- _1546.java
+++ _1546.java
@@ -1,7 +1,22 @@
 package baekjoon;
 
+import java.io.*;
+
 public class _1546 {
-	public static void main(String[] arg) {
+	public static void main(String[] arg) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] numbers = br.readLine().split(" ");
+		int sum = 0;
+		double max = Double.MIN_VALUE;
+		for (int i = 0; i < N; i++) {
+			sum += Integer.parseInt(numbers[i]);
+			if (max < Integer.parseInt(numbers[i])) {
+				max = Integer.parseInt(numbers[i]);
+			}
+		}
+
+		System.out.println(sum * 100 / max / N);
 
 	}
 }
