bc34b9116ccd24763d4a8755e2b7de9b8822dcf3
leejuhyeon <esloven3@gmail.com>
2022-03-14 18:53:47 +0900

    Update

--- _1075.java
+++ _1075.java
@@ -3,47 +3,35 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 public class _1075 {
 
-    static int N, F;
+	static int N, F;
 
-    public static void main(String[] arg) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine().trim());
-        F = Integer.parseInt(br.readLine().trim());
+	public static void main(String[] arg) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine().trim());
+		F = Integer.parseInt(br.readLine().trim());
+		
+		N = (N / 100) * 100;
 
-        if (N % F != 0) {
-
-            N = (N / 100) * 100;
-
-            for(int i = 0; i < 100; i++){
-                N += i;
-                if(N % F == 0){
-                    if(i < 10){
-                        if(i == 0){
-                            System.out.println("00");
-                        } else {
-                            System.out.println("0" + i);
-                        }
-                    } else{
-                        System.out.println(N % F);
-                    }
-                    break;
-                }
-            }
-        } else {
-            if(N % F == 0){
-                if(N % F < 10){
-                    if(N % F == 0){
-                        System.out.println("00");
-                    } else {
-                        System.out.println("0" + N % F);
-                    }
-                } else{
-                    System.out.println(N % F);
-                }
-            }
-        }
-    }
+		System.out.println(N);
+		System.out.println(F);
+		
+		for (int i = 0; i < 100; i++) {
+			int temp = N + i;
+			System.out.println(temp);
+			if (temp % F == 0) {
+				if (i < 10) {
+					if (i == 0) {
+						System.out.println("00");
+					} else {
+						System.out.println("0" + i);
+					}
+				} else {
+					System.out.println(i);
+				}
+				break;
+			}
+		}
+	}
 }
